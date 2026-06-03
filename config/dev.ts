1ff110156147a415b95be5529ccfff05adb267c3
Tsoorh <tsoor.hartov@gmail.com>
2026-06-03 16:25:15 +0300

    fix: update mongodb version to 7.2.0 and clean up dev.ts configuration

--- config/dev.ts
+++ config/dev.ts
@@ -1,6 +1,7 @@
 import { Config } from ".";
 
 export default {
-    dbURL: process.env.MONGO_URL || "mongodb://172.30.80.1:27017",
+    // dbURL: process.env.MONGO_URL || "mongodb://172.30.80.1:27017",
+    dbURL: process.env.MONGO_URL ,
     dbName: "tiran_lasry_db"
 } as Config
