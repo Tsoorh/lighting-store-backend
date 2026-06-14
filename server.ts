586a3fc898bd7dc0026e2089e8a3487c747fb33e
Tsoorh <tsoor.hartov@gmail.com>
2026-06-14 18:16:24 +0300

    Fix environment check for CORS configuration to use production mode

--- server.ts
+++ server.ts
@@ -16,7 +16,7 @@ const app = express();
 app.set('trust proxy', 1);
 
 // **************config****************
-const isProd = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
 const corsOptions = {
   origin: isProd 
           ? [
